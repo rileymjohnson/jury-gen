be6579282299694f8d0a8ff24ff2340903f0ee95
Riley Johnson <rileymillerjohnson@gmail.com>
2025-11-22 12:48:32 -0500

    added api gateway terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,7 @@ output "jury_api_key" {
   sensitive   = true
 }
 
+output "api_base_url" {
+  description = "Base URL for the Jury App API"
+  value       = "https://${aws_api_gateway_rest_api.jury_api.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
+}
